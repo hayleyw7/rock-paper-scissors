65a2188ef7892fd39c68766ed55c2bf19628ca36
Hayley Witherell <hayleywitherell@gmail.com>
2021-04-21 17:22:09 -0600

    Add start of pseudocode to js files

---
+++ player.js
@@ -0,0 +1,6 @@
+// A player.js file that contains a Player class.
+// Player methods must include, but are not limited to:
+// constructor - properties should include: name (ex: 'hannah'), token (ex: '⭐️'), wins (ex: 0)
+// saveWinsToStorage
+// retrieveWinsFromStorage
+// takeTurn
