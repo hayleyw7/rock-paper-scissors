d1ffb370b87affc04b67e3d57c34e6c0cd7a0ecc
Hayley Witherell <hayleywitherell@gmail.com>
2021-04-24 09:32:45 -0600

    Move determine winner function to game.js

--- game.js
+++ game.js
@@ -21,13 +21,131 @@ class Game {
   //   this.format = format;
   //   this.turn = turn;
   // }
-  determineWinner() {
-    // if human won, they get a point
-    // else if pc won, they get a point
-    // else no points (draw)
+  determineWinner(choice) {
+    // RPS WINNER FUNCTION
+
+    if (this.type === "rps") {
+      computerScore.innerText = computer.wins;
+      humanScore.innerText = human.wins;
+
+      var rpsCompChoice = getRandomRpsChoice();
+      var rpsHumanChoice = choice;
+
+      // tie
+
+      if (rpsHumanChoice === rpsCompChoice) {
+        tieAlert();
+
+        // comp throws rock
+      } else if (rpsCompChoice === "rock" && rpsHumanChoice === "scissors") {
+        loseAlert();
+        computer.wins++;
+      } else if (rpsCompChoice === "rock" && rpsHumanChoice === "paper") {
+        winAlert();
+
+        // comp throws paper
+      } else if (rpsCompChoice === "paper" && rpsHumanChoice === "scissors") {
+        winAlert();
+        human.wins++;
+      } else if (rpsCompChoice === "paper" && rpsHumanChoice === "rock") {
+        loseAlert();
+        computer.wins++;
+
+        // comp throws scissors
+      } else if (rpsCompChoice === "scissors" && rpsHumanChoice === "rock") {
+        winAlert();
+        human.wins++;
+      } else if (rpsCompChoice === "scissors" && rpsHumanChoice === "paper") {
+        loseAlert();
+        computer.wins++;
+      }
+    } else {
+      // MTG WINNER FUNCTION
+
+      computerScore.innerText = computer.wins;
+      humanScore.innerText = human.wins;
+
+      var mtgCompChoice = getRandomMtgChoice();
+      var mtgHumanChoice = choice;
+
+      // tie
+
+      if (mtgHumanChoice === mtgCompChoice) {
+        tieAlert();
+
+        // comp throws rock
+      } else if (mtgCompChoice === "rock" && mtgHumanChoice === "scissors") {
+        computer.wins++;
+        loseAlert();
+      } else if (mtgCompChoice === "rock" && mtgHumanChoice === "paper") {
+        human.wins++;
+        winAlert();
+      } else if (mtgCompChoice === "rock" && mtgHumanChoice === "lizard") {
+        computer.wins++;
+        loseAlert();
+      } else if (mtgCompChoice === "rock" && mtgHumanChoice === "alien") {
+        human.wins++;
+        winAlert();
+
+        // comp throws paper
+      } else if (mtgCompChoice === "paper" && mtgHumanChoice === "scissors") {
+        human.wins++;
+        winAlert();
+      } else if (mtgCompChoice === "paper" && mtgHumanChoice === "rock") {
+        computer.wins++;
+        loseAlert();
+      } else if (mtgCompChoice === "paper" && mtgHumanChoice === "lizard") {
+        human.wins++;
+        winAlert();
+      } else if (mtgCompChoice === "paper" && mtgHumanChoice === "alien") {
+        computer.wins++;
+        loseAlert();
+
+        // comp throws scissors
+      } else if (mtgCompChoice === "scissors" && mtgHumanChoice === "rock") {
+        human.wins++;
+        winAlert();
+      } else if (mtgCompChoice === "scissors" && mtgHumanChoice === "paper") {
+        computer.wins++;
+        loseAlert();
+      } else if (mtgCompChoice === "scissors" && mtgHumanChoice === "lizard") {
+        computer.wins++;
+        loseAlert();
+      } else if (mtgCompChoice === "scissors" && mtgHumanChoice === "alien") {
+        human.wins++;
+        winAlert();
+
+        // comp throws lizard
+      } else if (mtgCompChoice === "lizard" && mtgHumanChoice === "rock") {
+        human.wins++;
+        winAlert();
+      } else if (mtgCompChoice === "lizard" && mtgHumanChoice === "paper") {
+        computer.wins++;
+        loseAlert();
+      } else if (mtgCompChoice === "lizard" && mtgHumanChoice === "scissors") {
+        human.wins++;
+        winAlert();
+      } else if (mtgCompChoice === "lizard" && mtgHumanChoice === "alien") {
+        computer.wins++;
+        loseAlert();
+
+        // comp throws alien
+      } else if (mtgCompChoice === "alien" && mtgHumanChoice === "paper") {
+        human.wins++;
+        winAlert();
+      } else if (mtgCompChoice === "alien" && mtgHumanChoice === "rock") {
+        computer.wins++;
+        loseAlert();
+      } else if (mtgCompChoice === "alien" && mtgHumanChoice === "lizard") {
+        human.wins++;
+        winAlert();
+      } else if (mtgCompChoice === "alien" && mtgHumanChoice === "scissors") {
+        computer.wins++;
+        loseAlert();
+      }
+    }
   }
-  resetGame() {
-  }
+  resetGame() {}
 }
 
 // class Game {
